178725fa20d06081da8d8d0fded47c8507213a9b
fangyi <498657135@qq.com>
2019-06-19 20:30:12 +0800

    代码结构优化

--- main.go
+++ main.go
@@ -1,34 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func main() {
 	//绑定请求和处理函数
-	http.HandleFunc("/user/login", func(writer http.ResponseWriter, request *http.Request) {
-		//解析参数
-		request.ParseForm()
-		mobile := request.PostForm.Get("mobile")
-		passwd := request.PostForm.Get("passwd")
-
-		loginok := false
-		if mobile == "18600000000" && passwd == "123456" {
-			loginok = true
-		}
-
-		//curl http://127.0.0.1:8989/user/login -X POST -d "mobile=18600000000&passwd=12345"
-		str := `{"code":0,"data":{"id":1,"token":'test'}}`
-		if !loginok {
-			str = `{"code":-1,"msg":"123123"}`
-		}
-		//设置header为JSON（默认是text/html）
-		writer.Header().Set("Content-Type", "appliction/json")
-		writer.WriteHeader(http.StatusOK)
-		//输出
-		writer.Write([]byte(str))
-	})
+	http.HandleFunc("/user/login", userLogin)
 
 	//搭建web服务器
 	http.ListenAndServe(":8989", nil)
+}
+
+func userLogin(writer http.ResponseWriter, request *http.Request) {
+	//解析参数
+	request.ParseForm()
+	mobile := request.PostForm.Get("mobile")
+	passwd := request.PostForm.Get("passwd")
+
+	loginok := false
+	if mobile == "18600000000" && passwd == "123456" {
+		loginok = true
+	}
+
+	//curl http://127.0.0.1:8989/user/login -X POST -d "mobile=18600000000&passwd=12345"
+	if loginok {
+		data := make(map[string]interface{})
+		data["id"] = 1
+		data["token"] = "success"
+		Resp(writer, 0, data, "登录成功")
+	} else {
+		Resp(writer, 0, nil, "账号或者密码错误")
+	}
+
+}
+
+type H struct {
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+	Msg  string      `json:"msg"`
+}
+
+func Resp(w http.ResponseWriter, code int, data interface{}, msg string) {
+	//设置header为JSON（默认是text/html）
+	w.Header().Set("Content-Type", "appliction/json")
+	w.WriteHeader(http.StatusOK)
+	//定义一个结构体
+	h := H{
+		Code: code,
+		Msg:  msg,
+		Data: data,
+	}
+	//将结构体转换成JSON输出
+	ret, err := json.Marshal(h)
+	if err != nil {
+		log.Println(err)
+	}
+	//输出
+	w.Write(ret)
 }
